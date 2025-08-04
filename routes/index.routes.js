f0842fd1ad988ff8c8bbbcb164399516462bd0ee
suraj <imsurajyadav044@gmail.com>
2025-08-05 00:20:19 +0530

    modified indexRouter's get route at / endpoint

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,14 +1,17 @@
 import express from "express"
 import productModel from "../models/product.js"
 import auth from "../middleware/auth.js"
+import cartItemModel from "../models/cartItem.js"
 
 export const indexRouter = express.Router()
 
 indexRouter.get('/', auth, async (req, res) => {
+    const cartItem = await cartItemModel.find({ userId: req.user.id })
+    console.log(cartItem)
     try {
         const items = await productModel.find()
         if (!items) return res.status(200).json({ message: 'empty' })
-        res.status(200).json(items)
+        res.status(200).json({ items, cartItem })
     } catch (error) {
         res.status(400).json({ error: error.message })
     }
